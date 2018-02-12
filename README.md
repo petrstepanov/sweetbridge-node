50f67a60e4a2a4793547f1d1394efcc22723bc2e
petrstepanov <stepanovps@gmail.com>
2018-02-12 12:44:50 -0500

    Minor changes

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Landing page for Sweetbridge, Inc.
 Go to [https://apps.twitter.com/](https://apps.twitter.com/). Sign in and click `Create New App` button.
 Fill in the `Application Details` form. Just do Name, Description and Website fields.
 
-On the `Application Management` page go to `Keys and Access Tokens` tab. On the bottom of the page click the button `Create my Access Token` button. Copy `Consumer Key`, `Consumer Secret`, `Access Token` and `Access Token Secret` values and paste them into `routes/index.js` file, lines 8-11. Make sure no spaces and new line symbols are added before or after these strings.
+On the `Application Management` page go to `Keys and Access Tokens` tab. On the bottom of the page click the button `Create my Access Token` button. Copy `Consumer Key`, `Consumer Secret`, `Access Token` and `Access Token Secret` values and paste them into `routes/index.js` file, lines 9-12. Make sure no spaces and new line symbols are added before or after these strings.
 
-Number of recent Twitter mentions on the landing page is stored in `routes/index.js` file, line 15.
+Number of recent Twitter mentions on the landing page is stored in `routes/index.js` file, line 16.
--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,25 @@ var router = express.Router();
 
 var prettyjson = require('prettyjson');
 
+// Get twitter mentions
 var Twitter = require('twitter');
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+var twit = new Twitter({
+  consumer_key: '2vzKk5lHOP9rWU77aLDKiYO7F',
+  consumer_secret: 'WRVynNFoSV17Yp3rw3BQgQOvVdOFcKngFqDcEpSKOagZMK1VC7',
+  access_token_key: '294332999-PfUnJ8Sqo3WIzyhyXCWuRik0hWaVtS7IJtTpSAkc',
+  access_token_secret: 'isuhfQkF7en4WUuNW0T5hlKBs0aQTkRlAuetvAmk3bZZ4'
 });
 
-var twitterMentions;
-client.get('statuses/mentions_timeline', {"count": 10}, function(error, tweets, response) {
-  if(error) throw error;
-  // console.log(prettyjson.render(tweets));  // The mentions.
-  // console.log(response);  // Raw response object.
-  twitterMentions = tweets;
-});
-
-/* GET home page. */
+// Get home page
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    mentions: twitterMentions
+  twit.get('statuses/mentions_timeline', {"count": 10}, function(error, tweets, response) {
+    if(error) throw error;
+    // console.log(prettyjson.render(tweets));
+    // console.log(response);
+    res.render('index', {
+      title: 'Sweetbridge. The economy of possible',
+      mentions: tweets
+    });
   });
 });
 
